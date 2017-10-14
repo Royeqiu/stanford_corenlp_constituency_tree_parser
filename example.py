381e7c4cc5edc45d6785ab456388b2e1d89475fd
Royeqiu <roye1202@hotmail.com.tw>
2017-10-14 02:23:15 -0700

    Upload an example

---
+++ example.py
@@ -0,0 +1,11 @@
+import TreeParser
+
+sentence='(ROOT (NP (NP (NP (NP (NN Power)) (CC and) (NP (NN elegance))) (ADVP (RB together))) (. .)))'
+tree_parser=Tree_Parser()
+Tree=tree_parser.create_tree(None,sentence)	
+print (sentence)
+print ('root\'s tag', Tree.tag)
+print ('root\'s tag', Tree.value)
+for child in Tree.children_nodes:
+    print ('child\'s tag: ',child.tag)
+    print ('child\'s value: ',child.value)
